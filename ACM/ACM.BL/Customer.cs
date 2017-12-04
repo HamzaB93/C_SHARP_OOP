46296081290f28cb6a9febacfa8b6bb84c2af061
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-12-04 14:49:35 +0000

    Define Customer class with properties

---
+++ ACM/ACM.BL/Customer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string lastName;
+        public string LastName
+        {
+            get
+            {
+                // any extra code
+                return LastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        // Use this syntax when you dont need to add extra code to get and set
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        // propg snippet to make the setter private so nothing else can change the property
+        public int CustomerId { get; private set; }
+
+        // Spec wanted to have a fullname 
+        // No backing field as we dont need storage, its using it for last and first name anyway
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+    }
+}
