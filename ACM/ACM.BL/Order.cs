e1878f57be79c7f4f236064a0cfba71dea85e3b6
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-12-05 09:50:22 +0000

    Create more classes

---
+++ ACM/ACM.BL/Order.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Order
+    {
+        // Properties
+        public DateTimeOffset? OrderDate { get; set; }
+        public int OrderId { get; private set; }
+
+        // Constructors
+
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            this.OrderId = orderId;
+        }
+
+        // Methods
+        public Order Retrieve(int orderId)
+        {
+            return new Order();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderDate == null)
+                isValid = false;
+
+            return isValid;
+        }
+    }
+}
