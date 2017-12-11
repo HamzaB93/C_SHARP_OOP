d9e8fbea856da23b6c31dfa82b51615743c77e9b
Hamza Bhatti <hamzabhatti93@gmail.com>
2017-12-11 10:31:16 +0000

    Create classes for order display and display item
    Classes are single purpose and are used through the OrderRepository Class

---
+++ ACM/ACM.BL/OrderDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderDisplay
+    {
+        // Properties needed to display an order
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset OrderDate { get; set; }
+        // COmposition
+        public List<OrderDisplayItem> OrderDisplayItemList { get; set; }
+        public int OrderId { get; set; } 
+        public Address ShippingAddress { get; set; }
+    }
+}
--- ACM/ACM.BL/OrderRepository.cs
+++ ACM/ACM.BL/OrderRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,54 @@ namespace ACM.BL
 {
     public class OrderRepository
     {
+        public OrderDisplay RetrieveOrderDisplay(int orderId)
+        {
+            OrderDisplay orderDisplay = new OrderDisplay();
+
+            // Temp hard coded retrieval
+            if (orderId == 10)
+            {
+                orderDisplay.FirstName = "Bilbo";
+                orderDisplay.LastName = "Baggins";
+                orderDisplay.OrderDate = new DateTimeOffset(2014, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+                orderDisplay.ShippingAddress = new Address()
+                {
+                    AddressType = 1,
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+
+                };
+            } 
+
+            orderDisplay.OrderDisplayItemList = new List<OrderDisplayItem>();
+
+            // Temp hard coded 
+            if (orderId == 10)
+            {
+                var orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "Sunflowers",
+                    PurchasePrice = 15.96M,
+                    OrderQuantity = 2
+                };
+                orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+
+                orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "Rake",
+                    PurchasePrice = 6M,
+                    OrderQuantity = 1
+                };
+                orderDisplay.OrderDisplayItemList.Add(orderDisplayItem);
+            }
+
+            return orderDisplay;
+        }
+
         // Retreive an order, temp hard coding for testing
         public Order Retreive(int orderItemId)
         {
